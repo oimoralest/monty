68a0e08d44d2e67fc7e75f19c645e3c4ab4996b8
David Arias <gedafu2005@gmail.com>
2020-09-18 04:28:22 +0000

    Betty

--- init_vars.c
+++ init_vars.c
@@ -6,7 +6,7 @@
  */
 int init_vars(info *fd_flags)
 {
-    fd_flags->fd_open = NULL;
+	fd_flags->fd_open = NULL;
 	fd_flags->linecounter = 0;
 	fd_flags->queueFlag = 0;
 	fd_flags->integer = NULL;
@@ -15,5 +15,5 @@ int init_vars(info *fd_flags)
 	fd_flags->buffer = NULL;
 	fd_flags->lines = NULL;
 	fd_flags->exec_opcode = NULL;
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
